783e0d8789b7b7fbb004634f44da93d7202c7e3c
Charlie Merland <charlie.merland@gmail.com>
2014-03-05 18:57:03 +0100

    Most Rated Movies Widget update to display half stars

--- public/views/most-rated-movies-widget.php
+++ public/views/most-rated-movies-widget.php
@@ -22,16 +22,16 @@ $movies = new WP_Query(
 		<div class="most-rated-movies">
 <?php while ( $movies->have_posts() ) :
 		 $movies->the_post();
-		 $thumbnail = get_the_post_thumbnail( get_the_ID(), 'thumbnail' );
-		 $thumbnail = ( '' != $thumbnail ? $thumbnail : sprintf( '<img src="%s" alt="%s" width="%d" height="%d" />', plugins_url( '../assets/no_poster.png', __FILE__ ), get_the_title(), 150, 150 ) );
-		 $rating    = get_post_meta( get_the_ID(), '_wpml_movie_rating', true );
-		 $rating    = ( '' != $rating ? "stars-$rating" : 'stars-0' );
+		 $thumbnail  = get_the_post_thumbnail( get_the_ID(), 'thumbnail' );
+		 $thumbnail  = ( '' != $thumbnail ? $thumbnail : sprintf( '<img src="%s" alt="%s" width="%d" height="%d" />', plugins_url( '../assets/no_poster.png', __FILE__ ), get_the_title(), 150, 150 ) );
+		 $rating     = get_post_meta( get_the_ID(), '_wpml_movie_rating', true );
+		 $rating_str = ( '' == $rating ? "stars_0_0" : 'stars_' . str_replace( '.', '_', $rating ) );
 ?>
 			<a href="<?php the_permalink(); ?>" title="<?php printf( '%s %s', __( 'Read more about', 'wpml' ), get_the_title() ); ?>">
 				<figure id="movie-<?php the_ID(); ?>" class="most-rated-movie">
 					<?php echo $thumbnail; ?>
 <?php if ( 'no' != $display_rating ) : ?>
-					<div class="movie_rating_display <?php echo $rating . ' ' . $display_rating; ?>"></div>
+					<div class="movie_rating_display <?php echo $rating_str . ' ' . $display_rating ?>"><?php if ( 'below' == $display_rating ) echo '<small>' . $rating . '/5</small>' ?></div>
 <?php endif; ?>
 				</figure>
 			</a>
