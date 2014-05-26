3301cd8f6602b758638173c677b484b6a330afe0
Charlie Merland <charlie.merland@gmail.com>
2014-05-26 11:07:22 +0200

    Settings view minor changes (anchors)

---
+++ admin/settings/views/page-settings-section-headers.php
@@ -0,0 +1,1 @@
+	<span id="<?php echo str_replace( 'wpml_settings-', 'wpml_section_', $section['id'] ) ?>"></span>
--- admin/settings/views/page-settings.php
+++ admin/settings/views/page-settings.php
@@ -11,13 +11,13 @@
 		<form action="options.php" method="post">
 
 			<ul class="wpml-tabs-nav">
-			    <li class="wpml-tabs-nav<?php if ( 'api' == $_section || '' == $_section ) echo ' active'; ?>"><a href="#" data-section="wpml_section=api"><h4><?php _e( 'TMDb API', WPML_SLUG ); ?></h4></a></li>
-			    <li class="wpml-tabs-nav<?php if ( 'movies' == $_section ) echo ' active'; ?>"><a href="#" data-section="wpml_section=movies"><h4><?php _e( 'Movies', WPML_SLUG ); ?></h4></a></li>
-			    <li class="wpml-tabs-nav<?php if ( 'images' == $_section ) echo ' active'; ?>"><a href="#" data-section="wpml_section=images"><h4><?php _e( 'Images', WPML_SLUG ); ?></h4></a></li>
-			    <li class="wpml-tabs-nav<?php if ( 'taxonomies' == $_section ) echo ' active'; ?>"><a href="#" data-section="wpml_section=taxonomies"><h4><?php _e( 'Taxonomies', WPML_SLUG ); ?></h4></a></li>
-			    <li class="wpml-tabs-nav<?php if ( 'deactivate' == $_section ) echo ' active'; ?>"><a href="#" data-section="wpml_section=deactivate"><h4><?php _e( 'Deactivate', WPML_SLUG ); ?></h4></a></li>
-			    <li class="wpml-tabs-nav<?php if ( 'uninstall' == $_section ) echo ' active'; ?>"><a href="#" data-section="wpml_section=uninstall"><h4><?php _e( 'Uninstall', WPML_SLUG ); ?></h4></a></li>
-			    <li class="wpml-tabs-nav<?php if ( 'maintenance' == $_section ) echo ' active'; ?>"><a href="#" data-section="wpml_section=maintenance"><h4><?php _e( 'Maintenance', WPML_SLUG ); ?></h4></a></li>
+			    <li class="wpml-tabs-nav<?php if ( 'api' == $_section || '' == $_section ) echo ' active'; ?>"><a href="<?php echo admin_url( 'admin.php?page=wpml_edit_settings&wpml_section=tmdb#wpml_section_tmdb' ) ?>" data-section="wpml_section=api"><h4><?php _e( 'TMDb API', WPML_SLUG ); ?></h4></a></li>
+			    <li class="wpml-tabs-nav<?php if ( 'movies' == $_section ) echo ' active'; ?>"><a href="<?php echo admin_url( 'admin.php?page=wpml_edit_settings&wpml_section=wpml#wpml_section_wpml' ) ?>" data-section="wpml_section=movies"><h4><?php _e( 'Movies', WPML_SLUG ); ?></h4></a></li>
+			    <li class="wpml-tabs-nav<?php if ( 'images' == $_section ) echo ' active'; ?>"><a href="<?php echo admin_url( 'admin.php?page=wpml_edit_settings&wpml_section=images#wpml_section_images' ) ?>" data-section="wpml_section=images"><h4><?php _e( 'Images', WPML_SLUG ); ?></h4></a></li>
+			    <li class="wpml-tabs-nav<?php if ( 'taxonomies' == $_section ) echo ' active'; ?>"><a href="<?php echo admin_url( 'admin.php?page=wpml_edit_settings&wpml_section=taxonomies#wpml_section_taxonomies' ) ?>" data-section="wpml_section=taxonomies"><h4><?php _e( 'Taxonomies', WPML_SLUG ); ?></h4></a></li>
+			    <li class="wpml-tabs-nav<?php if ( 'deactivate' == $_section ) echo ' active'; ?>"><a href="<?php echo admin_url( 'admin.php?page=wpml_edit_settings&wpml_section=deactivate#wpml_section_deactivate' ) ?>" data-section="wpml_section=deactivate"><h4><?php _e( 'Deactivate', WPML_SLUG ); ?></h4></a></li>
+			    <li class="wpml-tabs-nav<?php if ( 'uninstall' == $_section ) echo ' active'; ?>"><a href="<?php echo admin_url( 'admin.php?page=wpml_edit_settings&wpml_section=uninstall#wpml_section_uninstall' ) ?>" data-section="wpml_section=uninstall"><h4><?php _e( 'Uninstall', WPML_SLUG ); ?></h4></a></li>
+			    <li class="wpml-tabs-nav<?php if ( 'maintenance' == $_section ) echo ' active'; ?>"><a href="<?php echo admin_url( 'admin.php?page=wpml_edit_settings&wpml_section=maintenance#wpml_section_maintenance' ) ?>" data-section="wpml_section=maintenance"><h4><?php _e( 'Maintenance', WPML_SLUG ); ?></h4></a></li>
 			</ul>
 
 <?php settings_fields( 'wpml_edit_settings' ); ?>
@@ -27,6 +27,7 @@
 <?php do_settings_sections( 'wpml_settings' ); ?>
 
 				<h3><?php _e( 'Maintenance', WPML_SLUG ); ?></h3>
+				<span id="wpml_section_maintenance"></span>
 				<table class="form-table" style="display: none;">
 					<tbody>
 						<tr>
