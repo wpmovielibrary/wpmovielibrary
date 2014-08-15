bcc2e8cbaa2aae5ecc3ed636f8cb4f9fb274de83
Charlie Merland <charlie.merland@gmail.com>
2014-08-16 01:11:45 +0200

    Custom Dashboard Vendor Widget styling

--- views/dashboard-vendor/vendor.php
+++ views/dashboard-vendor/vendor.php
@@ -1,9 +1,15 @@
 
 							<div class="main">
-								<p><?php _e( 'Use it, love it, rate it!', 'wpmovielibrary' ) ?></p>
-								<p><a href="http://wordpress.org/support/view/plugin-reviews/wpmovielibrary?rate=5#postform"><?php _e( 'Give us five shiny stars!', 'wpmovielibrary' ) ?></a></p>
-								<p><?php _e( 'We are social guys, share with us:', 'wpmovielibrary' ) ?></p>
-								<ul>
+								<p class="title">
+									<?php _e( 'Use it, love it, rate it!', 'wpmovielibrary' ) ?>
+								</p>
+								<p class="rateme">
+									<a href="http://wordpress.org/support/view/plugin-reviews/wpmovielibrary?rate=5#postform" title="<?php _e( 'Give us five shiny stars!', 'wpmovielibrary' ) ?>">
+										<span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span>
+									</a>
+								</p>
+								<p class="joinus"><?php _e( 'We\'re social guys, join us:', 'wpmovielibrary' ) ?><br /><small><?php _e( 'We have cookies!', 'wpmovielibrary' ) ?></small></p>
+								<ul class="social">
 									<?php echo $links ?>
 								</ul>
 							</div>
