49c0507597320f0cfa85b3210345cd987063d2e0
Charlie Merland <charlie@caercam.org>
2014-09-29 01:58:31 +0200

    Basic languages config file

---
+++ includes/config/wpmoly-languages.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * WPMovieLibrary Config Languages definition
+ *
+ * @package   WPMovieLibrary
+ * @author    Charlie MERLAND <charlie@caercam.org>
+ * @license   GPL-3.0
+ * @link      http://www.caercam.org/
+ * @copyright 2014 Charlie MERLAND
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) )
+	wp_die();
+
+$wpmoly_languages = array();
+
+// 	'' => array(
+// 		'native' => '',
+// 		'name'   => __( '', 'wpmovielibrary' )
+// 	),
+
+	'af' => array(
+		'native' => 'Afrikaans',
+		'name'   => __( 'Afrikaans', 'wpmovielibrary' )
+	),
+	'ar' => array(
+		'native' => 'العربية',
+		'name'   => __( 'Arabic', 'wpmovielibrary' )
+	),
+	'bg' => array(
+		'native' => 'български език',
+		'name'   => __( 'Bulgarian', 'wpmovielibrary' )
+	),
+	'cs' => array(
+		'native' => 'Český',
+		'name'   => __( 'Czech', 'wpmovielibrary' )
+	),
+	'da' => array(
+		'native' => 'Dansk',
+		'name'   => __( 'Danish', 'wpmovielibrary' )
+	),
+	'de' => array(
+		'native' => 'Deutsch',
+		'name'   => __( 'German', 'wpmovielibrary' )
+	),
+	'el' => array(
+		'native' => 'ελληνικά',
+		'name'   => __( '', 'wpmovielibrary' )
+	),
+	'en' => array(
+		'native' => 'English',
+		'name'   => __( 'English', 'wpmovielibrary' )
+	),
+	'es' => array(
+		'native' => 'Español',
+		'name'   => __( 'Spanish', 'wpmovielibrary' )
+	),
+	'fa' => array(
+		'native' => 'فارسی',
+		'name'   => __( 'Farsi', 'wpmovielibrary' )
+	),
+	'fi' => array(
+		'native' => 'Suomi',
+		'name'   => __( 'Finnish', 'wpmovielibrary' )
+	),
+	'fr' => array(
+		'native' => 'Français',
+		'name'   => __( 'French', 'wpmovielibrary' )
+	),
+	'he' => array(
+		'native' => 'עִבְרִית',
+		'name'   => __( 'Hebrew', 'wpmovielibrary' )
+	),
+	'hi' => array(
+		'native' => 'हिन्दी',
+		'name'   => __( 'Hindi', 'wpmovielibrary' )
+	),
+	'hu' => array(
+		'native' => 'Magyar',
+		'name'   => __( '', 'wpmovielibrary' )
+	),
+	'it' => array(
+		'native' => 'Italiano',
+		'name'   => __( 'Italian', 'wpmovielibrary' )
+	),
+	'ja' => array(
+		'native' => '日本語',
+		'name'   => __( 'Japanese', 'wpmovielibrary' )
+	),
+	'ko' => array(
+		'native' => '한국어/조선어',
+		'name'   => __( 'Korean', 'wpmovielibrary' )
+	),
+	'nb' => array(
+		'native' => 'Bokmål',
+		'name'   => __( 'Norwegian Bokmål', 'wpmovielibrary' )
+	),
+	'nl' => array(
+		'native' => 'Nederlands',
+		'name'   => __( 'Dutch', 'wpmovielibrary' )
+	),
+	'no' => array(
+		'native' => 'Norsk',
+		'name'   => __( 'Norwegian', 'wpmovielibrary' )
+	),
+	'ny' => array(
+		'native' => 'chiCheŵa, chinyanja',
+		'name'   => __( 'Chichewa', 'wpmovielibrary' )
+	),
+	'pl' => array(
+		'native' => 'Polski',
+		'name'   => __( 'Polish', 'wpmovielibrary' )
+	),
+	'pt' => array(
+		'native' => 'Português',
+		'name'   => __( 'Portuguese', 'wpmovielibrary' )
+	),
+	'ru' => array(
+		'native' => 'Pусский',
+		'name'   => __( 'Russian', 'wpmovielibrary' )
+	),
+	'sk' => array(
+		'native' => 'Slovenčina',
+		'name'   => __( 'Slovak', 'wpmovielibrary' )
+	),
+	'st' => array(
+		'native' => 'Sesotho',
+		'name'   => __( 'Southern Sotho', 'wpmovielibrary' )
+	),
+	'sv' => array(
+		'native' => 'Svenska',
+		'name'   => __( 'Swedish', 'wpmovielibrary' )
+	),
+	'ta' => array(
+		'native' => 'தமிழ்',
+		'name'   => __( 'Tamil', 'wpmovielibrary' )
+	),
+	'th' => array(
+		'native' => 'ภาษาไทย',
+		'name'   => __( 'Thai', 'wpmovielibrary' )
+	),
+	'tr' => array(
+		'native' => 'Türkçe',
+		'name'   => __( 'Turkish', 'wpmovielibrary' )
+	),
+	'uk' => array(
+		'native' => 'Український',
+		'name'   => __( 'Ukrainian', 'wpmovielibrary' )
+	),
+	'zh' => array(
+		'native' => '中国',
+		'name'   => __( 'Chinese', 'wpmovielibrary' )
+	),
+	'xh' => array(
+		'native' => 'isiXhosa',
+		'name'   => __( 'Xhosa', 'wpmovielibrary' )
+	),
+	'zu' => array(
+		'native' => 'isiZulu',
+		'name'   => __( 'Zulu', 'wpmovielibrary' )
+	)
+);
