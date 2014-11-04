6c39a8d2c17d6de50aad354bec0056bac776bbe2
Charlie Merland <charlie@caercam.org>
2014-11-04 23:55:40 +0100

    JS public fix on some themes

--- assets/js/public/wpmoly.js
+++ assets/js/public/wpmoly.js
@@ -1,34 +1,36 @@
 
-$ = $ || jQuery;
+(function($) {
 
-window.wpmoly = window.wpmoly || {};
+	window.wpmoly = window.wpmoly || {};
 
-	wpmoly.init = function() {
+		wpmoly.init = function() {
 
-		$( '.wpmoly-list' ).change(function() {
-			if ( this.options[ this.selectedIndex ].value.length > 0 )
-				location.href = this.options[ this.selectedIndex ].value;
-		});
+			$( '.wpmoly.list' ).change(function() {
+				if ( this.options[ this.selectedIndex ].value.length > 0 )
+					location.href = this.options[ this.selectedIndex ].value;
+			});
 
-		$( '.hide-if-js' ).hide();
-		$( '.hide-if-no-js' ).removeClass( 'hide-if-no-js' );
-	};
-
-	wpmoly.headbox = wpmoly_headbox = {};
-
-		wpmoly.headbox.toggle = function( item, post_id ) {
-
-			var $tab = $( '#movie-headbox-' + item + '-' + post_id ),
-			 $parent = $( '#movie-headbox-' + post_id ),
-			   $tabs = $parent.find( '.wpmoly.headbox.movie.content > .content' ),
-			   $link = $( '#movie-headbox-' + item + '-link-' + post_id );
-
-			if ( undefined != $tab ) {
-				$tabs.hide();
-				$tab.show();
-				$parent.find( 'a.active' ).removeClass( 'active' );
-				$link.addClass( 'active' );
-			}
+			$( '.hide-if-js' ).hide();
+			$( '.hide-if-no-js' ).removeClass( 'hide-if-no-js' );
 		};
 
-	wpmoly.init();
+		wpmoly.headbox = wpmoly_headbox = {};
+
+			wpmoly.headbox.toggle = function( item, post_id ) {
+
+				var $tab = $( '#movie-headbox-' + item + '-' + post_id ),
+				$parent = $( '#movie-headbox-' + post_id ),
+				$tabs = $parent.find( '.wpmoly.headbox.movie.content > .content' ),
+				$link = $( '#movie-headbox-' + item + '-link-' + post_id );
+
+				if ( undefined != $tab ) {
+					$tabs.hide();
+					$tab.show();
+					$parent.find( 'a.active' ).removeClass( 'active' );
+					$link.addClass( 'active' );
+				}
+			};
+
+		wpmoly.init();
+	
+})(jQuery);
