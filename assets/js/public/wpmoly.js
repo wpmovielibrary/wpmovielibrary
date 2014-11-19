b905f1f49239978959c1b82a75d8b7cc6ad49552
Charlie Merland <charlie@caercam.org>
2014-11-19 18:45:37 +0100

    Force grid view posters resizing in JS #151

--- assets/js/public/wpmoly.js
+++ assets/js/public/wpmoly.js
@@ -10,6 +10,9 @@
 					location.href = this.options[ this.selectedIndex ].value;
 			});
 
+			if ( undefined != $( '#wpmoly-movie-grid > .movie' ) )
+				wpmoly.grid_resize();
+
 			$( '.hide-if-js' ).hide();
 			$( '.hide-if-no-js' ).removeClass( 'hide-if-no-js' );
 		};
@@ -30,6 +33,30 @@
 					$link.addClass( 'active' );
 				}
 			};
+
+		wpmoly.grid_resize = function() {
+
+			var $movies = $( '#wpmoly-movie-grid > .movie' )
+			 max_height = 0,
+			  max_width = 0;
+
+			$.each( $movies, function() {
+				var $img = $( this ).find( 'img.wpmoly.grid.movie.poster' ),
+				   width = $img.width(),
+				  height = $img.height();
+
+				if ( height > max_height )
+					max_height = height;
+				if ( width > max_width )
+					max_width = width;
+
+			});
+
+			$movies.css({
+				height: Math.round( max_width * 1.33 ),
+				width: max_width
+			});
+		};
 
 		wpmoly.init();
 	
