561816f0bff4402b144fa0354dd7f8ebc026680f
Charlie Merland <charlie@caercam.org>
2014-12-29 20:04:07 +0100

    Update Grid style #196

--- views/movies/grid/loop.php
+++ views/movies/grid/loop.php
@@ -17,7 +17,7 @@ if ( ! empty( $movies ) ) :
 <?php 	if ( $title ) : ?>
 							<h4 class="wpmoly grid movie title"><?php the_title(); ?></h4>
 <?php 	endif; if ( $year ) : ?>
-							<span class="wpmoly grid movie genres"><?php echo apply_filters( 'wpmoly_format_movie_release_date', wpmoly_get_movie_meta( get_the_ID(), 'release_date' ), 'Y' ); ?></span>
+							<span class="wpmoly grid movie year"><?php echo apply_filters( 'wpmoly_format_movie_release_date', wpmoly_get_movie_meta( get_the_ID(), 'release_date' ), 'Y' ); ?></span>
 <?php 	endif; if ( $rating ) : ?>
 							<span class="wpmoly grid movie rating"><?php echo apply_filters( 'wpmoly_movie_rating_stars', wpmoly_get_movie_rating( get_the_ID() ) ); ?></span>
 <?php 	endif; ?>
