8b6d6c1430c2caedbb7fd684588fc1b7a14e1745
Charlie Merland <charlie@caercam.org>
2015-02-03 10:19:31 +0100

    Fix small default headbox theme bug

--- public/class-wpmoly-headbox.php
+++ public/class-wpmoly-headbox.php
@@ -52,7 +52,7 @@ if ( ! class_exists( 'WPMOLY_Headbox' ) ) :
 			if ( ! in_array( $theme, array_keys( $this->themes ) ) )
 				$theme = 'wpmoly';
 
-			if ( '' != $theme ) {
+			if ( ! empty( $theme ) && 'wpmoly' != $theme ) {
 
 				$theme = esc_attr( $theme );
 				$class = WPMOLY_PATH . "/public/class-wpmoly-headbox-$theme.php";
@@ -64,10 +64,10 @@ if ( ! class_exists( 'WPMOLY_Headbox' ) ) :
 					$class   = new $this->themes[ $theme ];
 					$headbox = $class->render( $content );
 				} else {
-					$headbox = self::get_wpmoly_headbox( $content );
+					$headbox = $this->get_wpmoly_headbox( $content );
 				}
 			} else {
-				$headbox = self::get_wpmoly_headbox( $content );
+				$headbox = $this->get_wpmoly_headbox( $content );
 			}
 
 			return $headbox;
@@ -119,18 +119,18 @@ if ( ! class_exists( 'WPMOLY_Headbox' ) ) :
 					$_item = '';
 					switch ( $item ) {
 						case 'rating':
-							$_item = apply_filters( 'wpmoly_movie_rating_stars', self::get_movie_meta( $id, 'rating' ) );
+							$_item = apply_filters( 'wpmoly_movie_rating_stars', wpmoly_get_movie_meta( $id, 'rating' ) );
 							$item  = 'rating starlined';
 							break;
 						case 'media':
 						case 'status':
-							$_item = apply_filters( "wpmoly_format_movie_$item", self::get_movie_meta( $id, $item ), $format = 'html', $icon = true );
+							$_item = apply_filters( "wpmoly_format_movie_$item", wpmoly_get_movie_meta( $id, $item ), $format = 'html', $icon = true );
 							break;
 						case 'release_date':
-							$_item = apply_filters( 'wpmoly_format_movie_year', self::get_movie_meta( $id, 'release_date' ), 'Y' );
+							$_item = apply_filters( 'wpmoly_format_movie_year', wpmoly_get_movie_meta( $id, 'release_date' ), 'Y' );
 							break;
 						default:
-							$_item = apply_filters( "wpmoly_format_movie_$item", self::get_movie_meta( $id, $item ) );
+							$_item = apply_filters( "wpmoly_format_movie_$item", wpmoly_get_movie_meta( $id, $item ) );
 							break;
 					}
 
