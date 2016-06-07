e8a80adf9bbf6172b3e33674cb74678f07320da9
Charlie Merland <charlie@caercam.org>
2016-06-07 19:58:30 +0200

    Minor movies template update

--- public/templates/shortcodes/movies.php
+++ public/templates/shortcodes/movies.php
@@ -17,12 +17,23 @@
 		<div class="wpmoly shortcode inline-block movie">
 
 			<div class="wpmoly shortcode poster">
-				<a href="<?php echo get_the_permalink( $movie->id ); ?>"><?php $movie->get_poster()->render( 'medium', 'html' ); ?></a>
+				<a href="<?php echo get_the_permalink( $movie->id ); ?>"><?php $movie->get_poster()->render( $poster, 'html' ); ?></a>
 			</div>
 
 			<a class="wpmoly shortcode link" href="<?php echo get_the_permalink( $movie->id ); ?>"><h4><?php $movie->meta->the_title(); ?></h4></a>
 
-			<div class="wpmoly shortcode meta"></div>
+			<div class="wpmoly shortcode meta">
+<?php
+	if ( ! empty( $meta ) ) :
+		foreach ( $meta as $slug ) :
+?>
+				<dt class="wpmoly shortcode meta <?php echo $slug ?> title"><?php echo $slug ?></dt>
+				<dd class="wpmoly shortcode meta <?php echo $slug ?> value"><?php $movie->meta->the( $slug ); ?></dd>
+<?php
+		endforeach;
+	endif;
+?>
+			</div>
 		</div>
 <?php endforeach; ?>
 
