4d821380003316778202085ef4a431412e35b299
Charlie Merland <charlie@caercam.org>
2019-08-14 00:18:37 +0200

    Tweak person extended headbox theme

--- public/templates/headboxes/person-extended.php
+++ public/templates/headboxes/person-extended.php
@@ -26,7 +26,7 @@
 			<div class="person-bio">
 				<div class="person-birthday"><?php printf( __( 'Born %s', 'wpmovielibrary' ), date_i18n( get_option( 'date_format', 'j, F Y' ), strtotime( $person->get_birthday() ) ) ); ?></div>
 				<div class="person-place-of-birth"><?php printf( __( 'In %s', 'wpmovielibrary' ), $person->get_the_place_of_birth() ); ?></div>
-				<div class="person-biography"><?php echo wp_trim_words( $person->get_biography(), 25, '…' ); ?></div>
+				<div class="person-biography"><?php $person->the_biography(); ?></div>
 			</div>
 		</div>
 		<div class="headbox-footer"></div>
