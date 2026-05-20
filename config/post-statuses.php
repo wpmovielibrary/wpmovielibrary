ef685cf296840cb87d079b01b04db7b43b8b7b24
Charlie Merland <charlie@caercam.org>
2026-05-20 23:40:33 +0200

    missing translation contexts in config/post-statuses.php

--- config/post-statuses.php
+++ config/post-statuses.php
@@ -2,7 +2,7 @@
 
 return [
 	'import-draft' => [
-		'label'                     => _x( 'Imported Draft', 'wpmovielibrary' ),
+		'label'                     => _x( 'Imported Draft', 'post import-draft status label', 'wpmovielibrary' ),
 		'public'                    => false,
 		'exclude_from_search'       => true,
 		'show_in_admin_all_list'    => false,
@@ -10,7 +10,7 @@ return [
 		'label_count'               => _n_noop( 'Imported Draft <span class="count">(%s)</span>', 'Imported Draft <span class="count">(%s)</span>' ),
 	],
 	'import-queued' => [
-		'label'                     => _x( 'Queued Movie', 'wpmovielibrary' ),
+		'label'                     => _x( 'Queued Movie', 'post import-queued status label', 'wpmovielibrary' ),
 		'public'                    => false,
 		'exclude_from_search'       => true,
 		'show_in_admin_all_list'    => false,
