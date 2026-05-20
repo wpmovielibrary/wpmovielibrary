b69c63338cf50a0a05be1c5f8c4c22af0dbbc466
Charlie Merland <charlie@caercam.org>
2026-05-20 17:41:11 +0200

    spaces → tabs

--- admin/templates/index.php
+++ admin/templates/index.php
@@ -1,21 +1,21 @@
 @extends('layout')
 
 @section('content')
-    <div class="wpmovielibrary-panels">
-        <div class="wpmovielibrary-panel">
-            <div class="wpmovielibrary-panel-header">
-                {!! esc_html( $post_type_label ) !!}
-                <div class="actions">
-                    <a href="{{ esc_url( admin_url( "post-new.php?post_type={$post_type}" ) ) }}" class="button button-primary">{{ esc_html( $post_type_labels->add_new_item ) }}</a>
-                </div>
-            </div>
-            <div class="wpmovielibrary-panel-content">
-                <div id="wpmovielibrary-index" class="wpmovielibrary wpmovielibrary-index wpmovielibrary-{{ esc_attr( $post_type ) }}-index" data-post-type="{{ esc_attr( $post_type ) }}" data-posts-per-page="{{ esc_attr( $posts_per_page ) }}" data-post-type-label="{{ $post_type_label }}" data-post-type-labels='{{ wp_json_encode( $post_type_labels ) }}'>
-                    <noscript>
-                        <div class="notice notice-error notice-alt"><p>wpMovieLibrary requires JavaScript. Please enable JavaScript in your browser settings.</p></div>
-                    </noscript>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="wpmovielibrary-panels">
+		<div class="wpmovielibrary-panel">
+			<div class="wpmovielibrary-panel-header">
+				{!! esc_html( $post_type_label ) !!}
+				<div class="actions">
+					<a href="{{ esc_url( admin_url( "post-new.php?post_type={$post_type}" ) ) }}" class="button button-primary">{{ esc_html( $post_type_labels->add_new_item ) }}</a>
+				</div>
+			</div>
+			<div class="wpmovielibrary-panel-content">
+				<div id="wpmovielibrary-index" class="wpmovielibrary wpmovielibrary-index wpmovielibrary-{{ esc_attr( $post_type ) }}-index" data-post-type="{{ esc_attr( $post_type ) }}" data-posts-per-page="{{ esc_attr( $posts_per_page ) }}" data-post-type-label="{{ $post_type_label }}" data-post-type-labels='{{ wp_json_encode( $post_type_labels ) }}'>
+					<noscript>
+						<div class="notice notice-error notice-alt"><p>wpMovieLibrary requires JavaScript. Please enable JavaScript in your browser settings.</p></div>
+					</noscript>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
